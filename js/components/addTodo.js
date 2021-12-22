86e42766751383b996e3f05d74efcdbb6dd9994b
Lucas Pintos <lucaspintos909@gmail.com>
2021-12-22 19:14:41 -0300

    feat: Importanbdo y usando componente de alerta

--- js/components/addTodo.js
+++ js/components/addTodo.js
@@ -1,14 +1,17 @@
+import Alert from "./alert.js";
+
 export default class AddTodo {
   constructor() {
     this.title = document.getElementById("title");
     this.description = document.getElementById("description");
     this.btn = document.getElementById("add");
+    this.alert = new Alert("alert");
   }
 
   onClick(callback) {
     this.btn.onclick = () => {
       if (title.value === "" || description.value === "") {
-        console.error("Title and description are required!");
+        this.alert.show("Title and description are required!");
       } else {
         callback({
           title: this.title.value,
