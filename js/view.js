c69ebc0bed99b80f301b046643d6c7d77f03d130
Lucas Pintos <lucaspintos909@gmail.com>
2021-12-22 18:39:44 -0300

    feat: Añadiendo checkbox con evento click

--- js/view.js
+++ js/view.js
@@ -16,40 +16,48 @@ export default class View {
   }
 
   addTodo({ title, description }) {
-    const todo = this.model.addTodo({title, description});
+    const todo = this.model.addTodo({ title, description });
     this.createRow(todo);
   }
 
-  removeTodo(id){
+  toggleCompleted(id){
+    this.model.toggleCompleted(id);
+  }
+
+  removeTodo(id) {
     this.model.removeTodo(id);
     document.getElementById(id).remove();
   }
 
-  createRow({id, title, description, completed}){
+  createRow({ id, title, description, completed }) {
     const row = this.table.insertRow();
 
-    row.setAttribute('id', id);
+    row.setAttribute("id", id);
 
     row.innerHTML = `
       <td>${title}</td>
       <td>${description}</td>
       <td class="text-center">
-        <input type="checkbox">
       </td>
       <td class="text-right">
         <button class="btn btn-primary mb-1">
           <i class="fa fa-pencil"></i>
         </button>
-        
       </td>
     `;
 
-    const removeBtn = document.createElement('button');
-    removeBtn.classList.add('btn', 'btn-danger', 'mb-1', 'ml-1');
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = completed;
+    checkbox.onclick = () => this.toggleCompleted(id);
+    row.children[2].appendChild(checkbox);
+
+    const removeBtn = document.createElement("button");
+    removeBtn.classList.add("btn", "btn-danger", "mb-1", "ml-1");
     removeBtn.innerHTML = '<i class="fa fa-trash"></i>';
     removeBtn.onclick = () => {
       this.removeTodo(id);
-    }
+    };
 
     row.children[3].appendChild(removeBtn);
   }
