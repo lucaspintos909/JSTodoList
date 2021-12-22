cd201c84bc4baada66004216fc0cf3bec4d2eaa5
Lucas Pintos <lucaspintos909@gmail.com>
2021-12-22 19:05:05 -0300

    feat: Funcion para guardar tareas en localstorage

--- js/model.js
+++ js/model.js
@@ -1,8 +1,26 @@
 export default class Model {
   constructor() {
     this.view = null;
-    this.todos = [];
-    this.currentId = 1;
+    this.todos = JSON.parse(localStorage.getItem("todos"));
+
+    if (!this.todos || this.todos.length < 1) {
+      this.todos = [
+        {
+          id: 0,
+          title: "Ejemplo",
+          description: "Tarea de ejemplo",
+          completed: false,
+        },
+      ];
+      this.currentId = 1;
+    } else {
+      this.currentId = this.todos[this.todos.length - 1].id + 1;
+    }
+  }
+
+  save() {
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+    console.log(this.todos);
   }
 
   setView(view) {
@@ -25,16 +43,21 @@ export default class Model {
       completed: false,
     };
     this.todos.push(todo);
+
+    this.save();
+
     return { ...todo };
   }
 
   removeTodo(id) {
     const index = this.findTodoById(id);
     this.todos.splice(index, 1);
+    this.save();
   }
 
   toggleCompleted(id) {
     const todo = this.todos[this.findTodoById(id)];
     todo.completed = !todo.completed;
+    this.save();
   }
 }
